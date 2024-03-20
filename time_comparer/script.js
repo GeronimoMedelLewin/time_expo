40e50395d2fa2dd4f8887f63271013e27d1f4209
GeronimoMedelLewin <136193923+GeronimoMedelLewin@users.noreply.github.com>
2024-03-20 18:28:02 -0300

    Add files via upload

--- time_comparer/script.js
+++ time_comparer/script.js
@@ -23,12 +23,22 @@ const Guion = document.getElementById("Guion");
 const mensaje2 = document.getElementById("text2");
 const Acita = document.getElementById("Acita");
 
+let currentTextIndex = 0;
+
 // const videoGuion = document.getElementById("Gvideo");
 
 const now = new Date();
-var min = now.getMinutes();
-var hor = now.getHours();
-var count = now.getSeconds();
+var min = 0;
+var hor = 0;
+var count = 0;
+/* function takeDate() {
+    if (currentTextIndex == 2) {*/
+        console.log("Hello World!");
+        min = now.getMinutes();
+        hor = now.getHours();
+        count = now.getSeconds();
+/*    }
+}*/
 
 var BmuteIsActive = false;
 var tickORtack = false;
@@ -38,21 +48,24 @@ tickTack.muted = true;
 let guion = ["¿Existe el Tiempo?", 
 "Esta experiencia tomará un poco de tu tiempo.", "Y para esto toma la hora del Sistema Operativo de tu ordenador o móvil.", "Compara ambas horas", 
 '"La distinción entre el pasado, el presente y el futuro es solo una ilusión obstinadamente persistente"', 
-"El tiempo humano no gira en forma circular. Corre hacia adelante en una línea recta. Es por esto que las personas no pueden ser felices: la felicidad busca la repetición", 
-"Por esta razón, su autor se preocupó de hacer una especie de imitación móvil de la eternidad y, mientras organizaba el cielo, hizo, a semejanza de la eternidad inmóvil y una, esta imagen eterna que progresa según las leyes de los Números, esto que nosotros llamamos el Tiempo", 
-"Escoger el propio tiempo es ganar tiempo", "Compara otra vez ambos relojes", 
+'"El tiempo humano no gira en forma circular. Corre hacia adelante en una línea recta. Es por esto que las personas no pueden ser felices: la felicidad busca la repetición"', 
+'"Por esta razón, su autor se preocupó de hacer una especie de imitación móvil de la eternidad y, mientras organizaba el cielo, hizo, a semejanza de la eternidad inmóvil y una, esta imagen eterna que progresa según las leyes de los Números, esto que nosotros llamamos el Tiempo"', 
+'"Es posible que no exista un movimiento uniforme con el cual medir exactamente el tiempo [absoluto]. Todos los movimientos pueden ser acelerados o retardados, pero el flujo del tiempo absoluto no puede ser alterado."', 
+'"Escoger el propio tiempo es ganar tiempo"', "Compara otra vez ambos relojes", 
 "Fracción de Tiempo"];
-let Cguion = ["Albert Einstein", "Milan Kundera", "Platon", "Sir Francis Bacon"];
-let CPguion = [5, 6, 7, 8];
+let Cguion = ["Albert Einstein", "Milan Kundera", "Platon", "Isaac Newton", "Sir Francis Bacon"];
+let CPguion = [5, 6, 7, 8, 9];
 let Tguion = [5000, 10000, 
-10000, 5000, 10000, 20000, 
-30000, 5000, 5000, 10000];
+10000, 5000, 10000, 15000, 
+20000, 15000, 5000, 5000, 10000];
 // var Dnumber = 0;
 // var DTnumber = 0;
 
 // Guion.textContent = guion[Dnumber];
 
-let currentTextIndex = 0;
+brakeT = false;
+
+var velR = 780;
 
 function showText() {
   if (currentTextIndex < guion.length) {
@@ -68,6 +81,7 @@ function showText() {
     setTimeout(() => {
       currentTextIndex++;
       showText();
+      // takeDate();
     }, Tguion[currentTextIndex]);
   }
 }
@@ -99,54 +113,72 @@ window.onload = function() {
 };
 
 setInterval(() => {
-    /*ShourI.textContent = Shour.value;
-    SminI.textContent = Smin.value;
-    SsecI.textContent = Ssec.value;
-    SminCI.textContent = SminC.value;
-    SsecCI.textContent = SsecC.value;
-    velI.textContent = vel.value;
-    */
-    count = count + /*parseInt(Ssec.value)*/ 1;
-    if (count >= /*parseInt(SsecC.value)*/ 60) {
-        min = min + /*parseInt(Smin.value)*/1;
+    // if (currentTextIndex >= 2) {
+        // console.log("Hola Mundo!");
+        /*ShourI.textContent = Shour.value;
+        SminI.textContent = Smin.value;
+        SsecI.textContent = Ssec.value;
+        SminCI.textContent = SminC.value;
+        SsecCI.textContent = SsecC.value;
+        velI.textContent = vel.value;
+        */
+        count = count + /*parseInt(Ssec.value)*/ 1;
+        if (count >= /*parseInt(SsecC.value)*/ 60) {
+            min = min + /*parseInt(Smin.value)*/1;
         count = 0;
-    }
-    if (min >= /*parseInt(SminC.value)*/60) {
-        hor = hor + /*parseInt(Shour.value)*/1;
-        min = 0;
-    }
-    TdateChile.textContent = `${String(hor).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(count).padStart(2, '0')}`;
-    Bmute.onclick = function() {
-        if (!BmuteIsActive) {
-            tickTack.muted = false;
-            tickTack2.muted = false;
-            BmuteIsActive = true;
-        } else {
-            tickTack.muted = true;
-            tickTack2.muted = true;
-            BmuteIsActive = false;
         }
-        /* mensaje.style.animation = "JumpTop";
-        mensaje.style.animationFillMode = "forwards";
-        mensaje.style.animationDuration = "0.5s";
-        mensaje.style.animationTimingFunction = "easy-in";*/
-        mensaje.textContent = "";
-        // videoGuion.play();
-        showText();
-    } 
-    if (BmuteIsActive) {
-        if (tickORtack) {
-            tickTack.play();
-        } else {
-            tickTack2.play();
+        if (min >= /*parseInt(SminC.value)*/60) {
+            hor = hor + /*parseInt(Shour.value)*/1;
+            min = 0;
         }
-    }
-    if(!tickORtack){
-        tickORtack = true;
+        TdateChile.textContent = `${String(hor).padStart(2, '0')}:${String(min).padStart(2, '0')}:${String(count).padStart(2, '0')}`;
+        if (BmuteIsActive) {
+            if (tickORtack) {
+                tickTack.play();
+            } else {
+                tickTack2.play();
+            }
+        }
+        if(!tickORtack){
+            tickORtack = true;
+        } else {
+            tickORtack = false;
+        }
+    // }
+}, velR);
+Bmute.onclick = function() {
+    if (!BmuteIsActive) {
+        tickTack.muted = false;
+        tickTack2.muted = false;
+        BmuteIsActive = true;
     } else {
-        tickORtack = false;
+        tickTack.muted = true;
+        tickTack2.muted = true;
+        BmuteIsActive = false;
     }
-}, 750);
+    /* mensaje.style.animation = "JumpTop";
+    mensaje.style.animationFillMode = "forwards";
+    mensaje.style.animationDuration = "0.5s";
+    mensaje.style.animationTimingFunction = "easy-in";*/
+    mensaje.textContent = "";
+    // videoGuion.play();
+    setInterval(() => {
+        if (!brakeT) {
+            if (count >= 50 && count <= 59) {
+                mensaje.textContent = "Cargando...";
+            } else {
+                if (count >= 30 && count <= 50) {
+                    velR = 100700;
+                }
+                if (!brakeT){
+                    mensaje.textContent = "";
+                    showText();
+                    brakeT = true;
+                }
+            }
+        }
+    }, 5);
+}
 /* setInterval(() => {
     Guion.textContent = guion[Dnumber];
     if (CPguion.some(item => typeof item === 'number' && item === Dnumber + 1)) {
